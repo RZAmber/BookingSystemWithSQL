be4ed10501ec7c6437e917e3fccadf86d16017f3
xchong0731 <xchong1@stevens.edu>
2016-05-03 14:19:34 -0400

    demo

---
+++ JAVA/HotelBooking/bin/HomePage1.java
@@ -0,0 +1,189 @@
+package reserve;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*; 
+ 
+ 
+public class HomePage extends JFrame{ 
+	private String[] hotel_info= new String[]{"Contact information","Tel: (201)586-9124","Email: hello_world_hotle@gmail.com"}; 
+	private int mapX=400; 
+	private int mapY=200; 
+ 
+ 
+	public HomePage(){ 
+ 	//Whole windows 
+ 		super("HOTEL BOOKING SYSTEM"); 
+ 		Container c=getContentPane(); 
+ 		c.setLayout(new BorderLayout()); 
+ 		ImageIcon bg=new ImageIcon("building.jpg"); 
+ 		JLabel l=new JLabel(bg); 
+ 		l.setBounds(0,0,bg.getIconWidth(),bg.getIconHeight()); 
+ 		getLayeredPane().add(l,new Integer(Integer.MIN_VALUE)); 
+ 		((JPanel) c).setOpaque(false); 
+ 		
+ 		
+ 		//Title panel 
+ 		JPanel p1=new JPanel(); 
+ 		p1.setOpaque(false); 
+ 		JLabel label1=new JLabel("WELCOME TO HELLO WORLD HOTLE !"); 
+ 		label1.setForeground(Color.WHITE); 
+ 		label1.setFont(new Font("Zapfino", Font.BOLD,26)); 
+ 		p1.add(label1); 
+ 		c.add(BorderLayout.NORTH,p1); 
+ 	 
+ 	 
+ 	 
+ 		//user Operation panel 
+ 		JPanel p2=new JPanel(); 
+ 		p2.setOpaque(false); 
+ 		p2.setLayout(new FlowLayout(FlowLayout.LEFT)); 
+ 		// after user setting the date the font color turn black 
+ 		JTextField f1=new JTextField("Please choose Location",13); 
+ 		f1.setForeground(Color.LIGHT_GRAY); 
+ 		JTextField f2=new JTextField("Check in date",10); 
+ 		f2.setForeground(Color.LIGHT_GRAY); 
+ 		JTextField f3=new JTextField("Check out date",10); 
+ 		f3.setForeground(Color.LIGHT_GRAY); 
+ 		JComboBox s1=new JComboBox(); 
+ 		s1.setMaximumRowCount(2);
+ 		s1.addItem("Please Choose");
+ 		s1.addItem("Hoboken");
+ 		
+ 		s1.addActionListener(new ActionListener(){
+ 			public void actionPerformed(ActionEvent e){
+ 				String Location = (String)s1.getSelectedItem();
+ 				if(Location.equals("Hoboken")){
+ 					f1.setText("Hoboken");
+ 					f1.setForeground(Color.BLACK);
+ 				}
+ 				else if(Location.equals("Please Choose")){
+ 					f1.setText("Please Choose");
+ 					f1.setForeground(Color.BLACK);
+ 				}
+ 			}
+ 		});
+ 		
+ 		JComboBox s21=new JComboBox(); 
+ 		s21.addItem("Year");
+ 		for (int i = 2016; i <= 2050; i++)
+ 			s21.addItem(i);
+ 		JComboBox s22 = new JComboBox();
+ 		s22.addItem("Month");
+ 		for (int i = 1; i <= 12; i++)
+ 			s22.addItem(i);
+ 		JComboBox s23 = new JComboBox();
+ 		s23.addItem("Day");
+ 		for (int i = 1; i <= 31; i++)
+ 			s23.addItem(i);
+ 		s21.addActionListener(new ActionListener(){
+ 			public void actionPerformed(ActionEvent e){
+ 				f2.setText(s21.getSelectedItem() + " " + s22.getSelectedItem()+ " " +s23.getSelectedItem());
+ 				f2.setForeground(Color.BLACK);
+ 			}
+ 		});
+ 		s22.addActionListener(new ActionListener(){
+ 			public void actionPerformed(ActionEvent e){
+ 				f2.setText(s21.getSelectedItem() + " " + s22.getSelectedItem()+ " " +s23.getSelectedItem());
+ 				f2.setForeground(Color.BLACK);
+ 			}
+ 		});
+ 		s23.addActionListener(new ActionListener(){
+ 			public void actionPerformed(ActionEvent e){
+ 				f2.setText(s21.getSelectedItem() + " " + s22.getSelectedItem()+ " " +s23.getSelectedItem());
+ 				f2.setForeground(Color.BLACK);
+ 			}
+ 		});
+ 	
+ 		JComboBox s31=new JComboBox(); 
+ 		s31.addItem("Year");
+ 		for (int i = 2016; i <= 2050; i++)
+ 			s31.addItem(i);
+ 		JComboBox s32 = new JComboBox();
+ 		s32.addItem("Month");
+ 		for (int i = 1; i <= 12; i++)
+ 			s32.addItem(i);
+ 		JComboBox s33 = new JComboBox();
+ 		s33.addItem("Day");
+ 		for (int i = 1; i <= 31; i++)
+ 			s33.addItem(i);
+ 		s31.addActionListener(new ActionListener(){
+ 			public void actionPerformed(ActionEvent e){
+ 				f3.setText(s31.getSelectedItem() + " " + s32.getSelectedItem() + " " + s33.getSelectedItem());
+ 				f3.setForeground(Color.BLACK);
+ 			}
+ 		});
+ 		s32.addActionListener(new ActionListener(){
+ 			public void actionPerformed(ActionEvent e){
+ 				f3.setText(s31.getSelectedItem() + " " + s32.getSelectedItem() + " " + s33.getSelectedItem());
+ 				f3.setForeground(Color.BLACK);
+ 			}
+ 		});
+ 		s33.addActionListener(new ActionListener(){
+ 			public void actionPerformed(ActionEvent e){
+ 				f3.setText(s31.getSelectedItem() + " " + s32.getSelectedItem() + " " + s33.getSelectedItem());
+ 				f3.setForeground(Color.BLACK);
+ 			}
+ 		});
+ 		JButton b1=new JButton("OK"); 
+ 		p2.add(f1); 
+ 		p2.add(s1); 
+ 		p2.add(f2); 
+ 		p2.add(s21); 
+ 		p2.add(s22);
+ 		p2.add(s23);
+ 		p2.add(f3); 
+ 		p2.add(s31); 
+ 		p2.add(s32);
+ 		p2.add(s33);
+ 		p2.add(b1); 
+ 		c.add(BorderLayout.CENTER,p2); 
+ 		
+ 		//information panel 
+ 		JPanel p3=new JPanel(); 
+ 		p3.setOpaque(false); 
+ 		p3.setBackground(Color.blue); 
+ 		p3.setLayout(new FlowLayout()); 
+ 	 
+	 
+ /*		JPanel subp1 = new JPanel();  
+ 		subp1.setOpaque(false); 
+ 		ImageIcon map =new ImageIcon("map.png"); 
+ 		map.setImage(map.getImage().getScaledInstance(mapX,mapY,Image.SCALE_DEFAULT)); 
+ 		JLabel mapl=new JLabel(map); 
+ 		JScrollPane maps=new JScrollPane(mapl); 
+ 		subp1.add(maps); 
+ 	*/	
+	 
+ 		// hotel contact information panel 
+ 		JPanel subp2=new JPanel(); 
+ 		subp2.setOpaque(false); 
+ 		subp2.setLayout(new GridLayout(3,1,20,10)); 
+ 		for(int i=0; i<hotel_info.length;i++){ 
+ 			JLabel l2=new JLabel(hotel_info[i]); 
+ 			l2.setForeground(Color.WHITE); 
+ 			l2.setFont(new Font("Noteworthy", Font.ITALIC,18)); 
+ 			l2.setHorizontalAlignment(JLabel.TRAILING); 
+ 			subp2.add(l2); 
+ 		} 
+ 	 
+ 
+ 
+ 		//p3.add(subp1); 
+ 		p3.add(subp2); 
+ 		c.add(BorderLayout.SOUTH,p3); 
+ 		setSize(1250,700); 
+ 		setResizable(false); 
+ 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+ 		setVisible(true); 
+	}	 
+	 
+
+ 
+ 	public static void main(String[] args) { 
+ 		new HomePage(); 
+ 		 
+ 	}  
+ 
+ } 
---
+++ JAVA/HotelBooking/bin/checkpage1.java
@@ -0,0 +1,290 @@
+package reserve;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.*;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.GridLayout;
+import java.awt.Image;
+
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JTextArea;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.Color;
+
+public class checkpage extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	ImageIcon bg =new ImageIcon("spbackground.jpg");
+	resultpage rp = new resultpage();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					checkpage frame = new checkpage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public checkpage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 616, 530);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		
+		bg.setImage(bg.getImage().getScaledInstance(800,530,Image.SCALE_DEFAULT));
+		JLabel l=new JLabel(bg);
+		l.setBounds(0,0,bg.getIconWidth(),bg.getIconHeight());
+		getLayeredPane().add(l,new Integer(Integer.MIN_VALUE));
+		contentPane.setOpaque(false);
+		setResizable(false);
+		
+		
+		JPanel panel_2 = new JPanel();
+	    panel_2.setOpaque(false);
+		panel_2.setBounds(0, 27, 632, 479);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JLabel label = new JLabel("First name");
+		label.setBounds(53, 0, 138, 29);
+		panel_2.add(label);
+		
+		textField = new JTextField();
+		textField.setForeground(Color.LIGHT_GRAY);
+		textField.setText("First name");
+		textField.setColumns(10);
+		textField.setBounds(317, 0, 190, 29);
+		panel_2.add(textField);
+		
+		JLabel label_1 = new JLabel("Last name");
+		label_1.setBounds(53, 29, 119, 29);
+		panel_2.add(label_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setForeground(Color.LIGHT_GRAY);
+		textField_1.setText("Last name");
+		textField_1.setColumns(10);
+		textField_1.setBounds(317, 29, 190, 29);
+		
+		panel_2.add(textField_1);
+		
+		JLabel label_2 = new JLabel("ROOM TYPE");
+		label_2.setBounds(53, 58, 174, 29);
+		panel_2.add(label_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setForeground(Color.LIGHT_GRAY);
+		textField_2.setText("ROOME TPYE");
+		textField_2.setColumns(10);
+		textField_2.setBounds(317, 58, 190, 29);
+		panel_2.add(textField_2);
+		
+		JLabel label_3 = new JLabel("AMOUNT");
+		label_3.setBounds(53, 87, 147, 29);
+		panel_2.add(label_3);
+		
+		textField_3 = new JTextField();
+		textField_3.setForeground(Color.LIGHT_GRAY);
+		textField_3.setText("AMOUNT");
+		textField_3.setColumns(10);
+		textField_3.setBounds(317, 87, 190, 29);
+		panel_2.add(textField_3);
+		
+		JLabel label_4 = new JLabel("CHECK IN TIME");
+		label_4.setBounds(53, 116, 147, 29);
+		panel_2.add(label_4);
+		
+		textField_4 = new JTextField();
+		textField_4.setForeground(Color.LIGHT_GRAY);
+		textField_4.setText("MM/DD/YY");
+		textField_4.setColumns(10);
+		textField_4.setBounds(317, 116, 190, 29);
+		panel_2.add(textField_4);
+		
+		JLabel label_5 = new JLabel("CHECK OUT TIME");
+		label_5.setBounds(53, 145, 174, 29);
+		panel_2.add(label_5);
+		
+		textField_5 = new JTextField();
+		textField_5.setForeground(Color.LIGHT_GRAY);
+		textField_5.setText("MM/DD/YY");
+		textField_5.setColumns(10);
+		textField_5.setBounds(317, 145, 190, 29);
+		panel_2.add(textField_5);
+		
+		JLabel label_6 = new JLabel("CONTACT INFORMATION");
+		label_6.setFont(new Font("Lucida Grande", Font.BOLD, 16));
+		label_6.setBounds(181, 186, 234, 39);
+		panel_2.add(label_6);
+		
+		JLabel label_7 = new JLabel("PHONE NUMBER:");
+		label_7.setBounds(52, 237, 131, 16);
+		panel_2.add(label_7);
+		
+		JLabel label_8 = new JLabel("E-mail:");
+		label_8.setBounds(53, 265, 61, 16);
+		panel_2.add(label_8);
+		
+		textField_6 = new JTextField();
+		textField_6.setForeground(Color.LIGHT_GRAY);
+		textField_6.setText("PHONE NUMBER");
+		textField_6.setColumns(10);
+		textField_6.setBounds(315, 232, 192, 26);
+		panel_2.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setForeground(Color.LIGHT_GRAY);
+		textField_7.setText("E-MAIL");
+		textField_7.setColumns(10);
+		textField_7.setBounds(315, 260, 192, 26);
+		panel_2.add(textField_7);
+		
+		JLabel lblComments = new JLabel("COMMENTS：");
+		lblComments.setBounds(53, 312, 100, 16);
+		panel_2.add(lblComments);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(317, 312, 190, 81);
+		panel_2.add(textArea);
+		
+		JButton btnBack = new JButton("Back");
+		btnBack.setBounds(150, 432, 117, 29);
+		btnBack.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				setVisible(false);
+			}
+		});
+		panel_2.add(btnBack);
+		
+		
+		
+		JButton btnComfirm = new JButton("comfirm");
+		btnComfirm.setBounds(400, 432, 117, 29);
+		btnComfirm.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+				rp.setVisible(true);
+			}
+		});
+		panel_2.add(btnComfirm);
+				
+		JPanel panel = new JPanel();
+		panel.setOpaque(false);
+		panel.setBounds(0, -4, 632, 32);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel label_9 = new JLabel("CHECK OUT PAGE");
+		label_9.setFont(new Font("Lucida Grande", Font.PLAIN, 21));
+		label_9.setBounds(208, 6, 189, 26);
+		panel.add(label_9);
+	}
+	
+	
+	/*
+	 * data extract part
+	 */
+	
+	private String Fname;
+	private String Lname;
+	private String roomType;
+	private int amount;
+	private String checkIn;
+	private String checkOut;
+	private String phone;
+	private String email;
+	private String comments;
+	
+	//set information part:
+	public void setFname(String Fname){
+		this.Fname=Fname;	
+	}
+	
+	public void setLname(String Lname){
+		this.Lname=Lname;
+	}
+
+
+	public void setRoomeType(String roomeTpye){
+	this.roomType = roomType;
+	
+}
+
+	public void setAmount(int amount){
+	this.amount = amount;
+}
+
+	public void setPhone(String phone){
+	this.phone= phone;
+}
+	
+	public void setEmail(String email){
+		this.email=email;
+}
+	
+	public void setCheckIn(String checkIn){
+		this.checkIn= checkIn;
+}
+	
+	public void setCheckOut(String checkOut){
+		this.checkOut=checkOut;
+}
+	
+
+	//get information part:
+	public String getFname(){
+		return textField.getText();
+	}
+	
+	public String getLname(){
+		return textField_1.getText();
+	}
+	
+	public String getRoomType(){
+		return textField_2.getText();
+	}
+	
+	public String getAmount(){
+		return textField_3.getText();
+	}
+	
+	public String getPhone(){
+		return textField_4.getText();
+	}
+	
+	public String getEmail(){
+		return textField_5.getText();
+	}
+	
+	public String getComments(){
+		return textField_6.getText();
+	}
+}
