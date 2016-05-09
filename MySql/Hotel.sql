01be00680249a0e25b98daa6577d534ddbb32c8f
Amber <zhangrui9399@gmail.com>
2016-05-09 17:01:31 -0400

    final sql

--- MySql/Hotel.sql
+++ MySql/Hotel.sql
@@ -9,7 +9,7 @@
  Target Server Version : 50711
  File Encoding         : utf-8
 
- Date: 05/05/2016 10:30:51 AM
+ Date: 05/09/2016 16:58:31 PM
 */
 
 SET NAMES utf8;
@@ -34,7 +34,7 @@ CREATE TABLE `manager` (
 --  Records of `manager`
 -- ----------------------------
 BEGIN;
-INSERT INTO `manager` VALUES ('300710', '123456', 'Joe', 'Goran', 'jgoran@hello.com', '2032452123', '1'), ('420890', '123456', 'Kim', 'Lin', 'klin@hello.com', '2032918833', '0'), ('589402', '123456', 'Zeta', 'Becky', 'becky.zeta@hello.com', '2013987273', '0');
+INSERT INTO `manager` VALUES ('300710', '123', 'Joe', 'Goran', 'jgoran@hello.com', '2032452123', '1'), ('300711', '123', 'Jana', 'Bord', 'jbord@hello.com', '2013145323', '1'), ('420890', '1234567', 'Kim', 'Lin', 'klin@hello.com', '2032918833', '0'), ('589402', '123456', 'Zeta', 'Becky', 'becky.zeta@hello.com', '2013987273', '0');
 COMMIT;
 
 -- ----------------------------
@@ -49,37 +49,37 @@ CREATE TABLE `order_info` (
   `room_type` varchar(64) NOT NULL,
   `amount` int(11) NOT NULL,
   `room_number` int(30) DEFAULT NULL,
-  `check-in` date NOT NULL,
-  `check-out` date NOT NULL,
+  `check_in` varchar(64) NOT NULL,
+  `check_out` varchar(64) NOT NULL,
   `total` float(40,0) DEFAULT NULL,
   `comment` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`orderID`,`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 --  Records of `order_info`
 -- ----------------------------
 BEGIN;
-INSERT INTO `order_info` VALUES ('1', 'kunqi@gmail.com', 'Kun', 'Qi', 'Single', '1', null, '2016-05-03', '2016-05-04', null, null), ('2', 'kunqi@gmail.com', 'Jia', 'Qi', 'Single', '1', null, '2016-05-05', '2016-05-10', null, null), ('3', 'litingwang@stevens.edu', 'Liting', 'Wang', 'Double', '1', null, '2016-05-10', '2016-05-11', null, null);
+INSERT INTO `order_info` VALUES ('8', 'kunqi99@gmail.com', 'Kun', 'Wang', '   suite', '1', '303', '2016 9 5', '2016 9 6', '500', ''), ('13', 'rzhang21@stevens.edu', 'Rui', 'Zhang', 'Standard Room   ', '1', '104', '2016 6 4', '2016 6 8', '800', ''), ('19', 'jiaowang@stevens.edu', 'Jiao', 'Wang', '  deluxe single room ', '1', '202', '2017 1 1', '2017 2 1', '9300', ''), ('20', 'lgoodstein@gmail.com', 'Laurie', 'Goodstein', ' single room  ', '1', '101', '2016 6 3', '2016 6 5', '300', '');
 COMMIT;
 
 -- ----------------------------
---  Table structure for `room_baseinfo`
+--  Table structure for `room`
 -- ----------------------------
-DROP TABLE IF EXISTS `room_baseinfo`;
-CREATE TABLE `room_baseinfo` (
+DROP TABLE IF EXISTS `room`;
+CREATE TABLE `room` (
   `room_number` int(11) NOT NULL,
-  `room_style` varchar(30) NOT NULL,
+  `room_type` varchar(30) NOT NULL,
   `fee` float NOT NULL,
-  `max` int(11) NOT NULL,
+  `Available` varchar(64) DEFAULT NULL,
   PRIMARY KEY (`room_number`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- ----------------------------
---  Records of `room_baseinfo`
+--  Records of `room`
 -- ----------------------------
 BEGIN;
-INSERT INTO `room_baseinfo` VALUES ('101', 'Single', '150', '2'), ('102', 'Single', '150', '2'), ('103', 'Single', '150', '2'), ('104', 'Double', '200', '4'), ('105', 'Double', '200', '4'), ('106', 'Single', '150', '2'), ('107', 'Single', '150', '2'), ('201', 'Double', '200', '4'), ('202', 'Deluxe Single', '200', '2'), ('203', 'Deluxe Single', '200', '2'), ('204', 'Double', '200', '2'), ('205', 'Double', '200', '4'), ('206', 'Deluxe Single', '200', '2'), ('207', 'Double', '200', '4'), ('301', 'Family', '300', '5'), ('302', 'Family', '300', '5'), ('303', 'Family', '300', '5'), ('304', 'Deluxe Single', '200', '2');
+INSERT INTO `room` VALUES ('101', 'Single_Room', '150', 'No'), ('102', 'Single_Room', '150', '-'), ('103', 'Single_Room', '150', '-'), ('104', 'Standard_Room', '300', 'No'), ('105', 'Standard_Room', '300', 'No'), ('106', 'Single_Room', '150', '-'), ('107', 'Single_Room', '150', '-'), ('201', 'Standard_Room', '300', 'No'), ('202', 'Deluxe_Single_Room', '200', 'No'), ('203', 'Deluxe_Single_Room', '200', '-'), ('204', 'Standard_Room', '300', '-'), ('205', 'Standard_Room', '300', '-'), ('206', 'Deluxe_Single_Room', '200', '-'), ('207', 'Standard_Room', '300', '-'), ('301', 'Suite', '500', 'No'), ('302', 'Suite', '500', '-'), ('303', 'Suite', '500', 'No'), ('304', 'Deluxe_Single_Room', '200', '-');
 COMMIT;
 
 -- ----------------------------
@@ -105,7 +105,7 @@ CREATE TABLE `user` (
 --  Records of `user`
 -- ----------------------------
 BEGIN;
-INSERT INTO `user` VALUES ('cuiying@gmail.com', 'win99999', 'Cui', 'Ying', '2013243742', 'USA', '437 OGDEN', '', 'Jersey City', 'Nk', '07307'), ('rzhang21@steven.edu', 'win930909', 'RUI', 'ZHANG', '2013153265', 'USA', '437 ogden', '', 'jersey city', 'nj', '07307'), ('rzhang21@stevens.edu', 'win930909', 'Rui', 'Zhang', '2013153265', 'USA', '437 OGDEN', 'APT4', 'Jersey City', 'Nj', '07307');
+INSERT INTO `user` VALUES ('cuiying@gmail.com', 'win99999', 'Ying', 'Cui', '2013243742', 'USA', '437 OGDEN', '', 'Jersey City', 'Nk', '07307'), ('jiaowang@stevens.edu', 'win99999', 'Jiao', 'Wang', '201437483', 'USA', '437 Ogden', '', 'NJ', 'nj', '07307'), ('joeh@stevens.edu', '1234', 'John', 'LI', '2019392993', 'US', '8th', '', 'NYC', 'NY', '00930'), ('kunqi99@gmail.com', 'win930909', 'Kun', 'Wang', '2013992983', 'USA', '437 O', '', 'Jersey City', 'NJ', '07307'), ('lgoodstein@gmail.com', 'win9302093', 'Laurie', 'Goodstein', '202939201', 'usa', '45th', '', 'NYC', 'NY', '02938'), ('liushui@163.com', '1234839', 'Alicia', 'Parlapiano', '2019393847', 'USA', '65th', '', 'NYC', 'NY', '029303'), ('rzhang21@stevens.edu', 'win930909', 'Rui', 'Zhang', '201928921', 'USA', 'OGDE', '', 'JERSEY', 'NJ', '07307'), ('xusihui@stevens.edu', 'win99999', 'Xusihui', 'Chong', '2013882938', 'USA', 'Hoboken', '', 'Hoboken', 'NJ', '07030'), ('yoyo@gmail.com', 'win99999', 'Ya', 'Duan', '2019392829', 'China', 'Heze', '', 'heze', 'nj', '274700'), ('yuliang@gmail.com', 'win930909', 'Yu', 'Guo', '2013928845', 'China', 'Chongqing', '', 'Chongqing', 'Chongqing', '02930');
 COMMIT;
 
 SET FOREIGN_KEY_CHECKS = 1;
